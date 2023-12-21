1974fac57920e9266d86092a5a63064f50d7625a
GreyLeaf23 <christian.torres2894@gmail.com>
2023-12-21 19:17:18 -0400

    Task 8 - RESTful API actions for City objects.

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,7 +3,7 @@
 Module handles all default RestFul API actions for City objects
 """
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from models import storage
 from models.city import City
 from models.state import State
@@ -40,9 +40,9 @@ def delete_city(city_id):
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    storage.delete(city)
+    city.delete()
     storage.save()
-    return jsonify({}), 200
+    return make_response(jsonify({}), 200)
 
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
@@ -56,10 +56,9 @@ def create_city(state_id):
         abort(400, description="Not a JSON")
     if 'name' not in request.get_json():
         abort(400, description="Missing name")
-    city = City(name=request.get_json()['name'], state_id=state_id)
-    storage.new(city)
-    storage.save()
-    return jsonify(city.to_dict()), 201
+    city = City(**request.get_json(), state_id=state_id)
+    city.save()
+    return make_response(jsonify(city.to_dict()), 201)
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'],
@@ -72,8 +71,8 @@ def update_city(city_id):
     if not request.get_json():
         abort(400, description="Not a JSON")
     for key, value in request.get_json().items():
-        if key not in ['id', 'created_at', 'updated_at']:
+        if key not in ['id', 'state_id', 'created_at', 'updated_at']:
             setattr(city, key, value)
-    storage.save()
-    return jsonify(city.to_dict()), 200
+    city.save()
+    return make_response(jsonify(city.to_dict()), 200)
 # Task 8 - End
