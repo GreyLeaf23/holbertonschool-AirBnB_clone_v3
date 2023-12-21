6cd11048e3ad6e8e7becf825c0d6848b6ed3ae45
GreyLeaf23 <christian.torres2894@gmail.com>
2023-12-21 11:44:47 -0400

    Task 4 - Starting API. (Multiple folders)

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,5 +5,5 @@ from flask import Blueprint
 # Task 4 - Create Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from . import *
+from api.v1.views.index import *
 # Task 4 - End
