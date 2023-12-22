ecab5d452599b91a5e2c97bec5745db8fb7f8f09
GreyLeaf23 <christian.torres2894@gmail.com>
2023-12-22 08:12:26 -0400

    Task 5 - Endpoint retrieves the number of each objects by type.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """Index view for api.v1.views"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.user import User
+from models.review import Review
 
 
 # Task 4 - Create a route that returns a JSON
@@ -10,3 +17,16 @@ def status():
     """Return status"""
     return jsonify({"status": "OK"})
 # Task 4 - End
+
+
+# Task 5 - Endpoint retrieves number of each objects by type
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Return stats"""
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
+# Task 5 - End
